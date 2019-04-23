f8878fead41c3ddfe5d593ed7608d099cbe84b56
Minh Vo <36206248+minhdvo@users.noreply.github.com>
2019-04-23 09:11:08 -0400

    Create findMedianSortedArrays.py

---
+++ from_codesignal/findMedianSortedArrays.py
@@ -0,0 +1,52 @@
+# Median of Two Sorted Arrays
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+# You may assume nums1 and nums2 cannot be both empty.
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        nums1.extend(nums2)
+        nums1.sort()
+        n =len(nums1)        
+        
+        if n%2 ==1:
+            m = n/2
+            med = nums1[m]
+        else:
+            m = n/2
+            med = 0.5*(nums1[m-1]+nums1[m])
+            
+        return med
+
+## Solution
+def median(A, B):
+    m, n = len(A), len(B)
+    if m > n:
+        A, B, m, n = B, A, n, m
+    if n == 0:
+        raise ValueError
+
+    imin, imax, half_len = 0, m, (m + n + 1) / 2
+    while imin <= imax:
+        i = (imin + imax) / 2
+        j = half_len - i
+        if i < m and B[j-1] > A[i]:
+            # i is too small, must increase it
+            imin = i + 1
+        elif i > 0 and A[i-1] > B[j]:
+            # i is too big, must decrease it
+            imax = i - 1
+        else:
+            # i is perfect
+
+            if i == 0: max_of_left = B[j-1]
+            elif j == 0: max_of_left = A[i-1]
+            else: max_of_left = max(A[i-1], B[j-1])
+
+            if (m + n) % 2 == 1:
+                return max_of_left
