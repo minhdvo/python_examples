c7948d8cfb5632c4c2a7ff5e5a3d10b4105ba2c7
Minh Vo <36206248+minhdvo@users.noreply.github.com>
2019-07-11 09:12:38 -0400

    Create 1_Functions.js

---
+++ JS/1_Functions.js
@@ -0,0 +1,39 @@
+/* Task
+Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
+
+Code */
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
