020b38a38b4aa43fe31552a84d80914f497e0725
Minh Vo <36206248+minhdvo@users.noreply.github.com>
2019-07-16 10:57:48 -0400

    Create 4_Count_Objects.js

---
+++ JS/4_Count_Objects.js
@@ -0,0 +1,62 @@
+/* In this challenge, we learn about iterating over objects. 
+
+Task
+Complete the function in the editor. It has one parameter: an array, a , of objects. Each object in the array has two integer 
+properties denoted by x and y. The function must return a count of all such objects o in array a that satisfy o.x==o.y.
+
+Code */
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    var count = 0;
+    for (var i in objects) {
+        
+        if (objects[i].x == objects[i].y) {
+            count += 1;
+        }
+    }
+    return count
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
